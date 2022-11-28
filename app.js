cf07cf546e0f0cdae89a91cc9403a6d7a9d1088e
paullaster <paullasterokoth98@gmail.com>
2022-11-29 00:03:32 +0300

    commit find singletable

--- app.js
+++ app.js
@@ -312,14 +312,66 @@ app.get ( '/find/db/table/all', (req, res) => {
                 });
                 return;
             };
+            res
+            .status ( 200)
+            .json ( {
+                status: 'success',
+                data: rows,
+            });
+        });
+    });        
+});
+
+// FIND ALL TABLES IN A DATABASE:
+app.get ( '/find/db/table/one', (req, res) => {
+    if(!(req.body.database)) {
+        res
+        .status (404)
+        .json ( {
+            status: 'error',
+            error: 'Please enter a database name',
+        });
+        return;
+    };
+    db.query ( 'SHOW DATABASES', ( err, rows) => {
+        if ( err) {
+            res
+            .status ( 500 )
+            .json ( {
+                status: 'error',
+                error: err.message,
+            });
+            return;
+        };
+        const dbToBeModified = rows.filter ( (row) => {
+            return row.Database === req.body.database;
+        } );
+        db.query ( ` SHOW TABLES FROM ${dbToBeModified[0].Database}`, ( err, rows) => {
+            if ( err) {
+                res
+                .status ( 500)
+                .json ( {
+                    status: 'error',
+                    error: err.message,
+                });
+                return;
+            };
             const tableToBeModified = rows.filter ( (row) => {
                 for ( let prop in row ) {
                     return row[prop] === req.body.dbTable;
                 };
-            } );
+            });
+            res
+            .status ( 200)
+            .json ( {
+                status: 'success',
+                data: tableToBeModified,
+            });    
         });
     });        
 });
+
+
 
 //UPDATE TABLE:
 app.put ( '/update-todb', ( req, res) => {});
