bd7d10763ca98ef4a082f8b28c9d9860e017357d
paullaster <paullasterokoth98@gmail.com>
2022-11-28 22:22:35 +0300

    adding multiple insert

--- app.js
+++ app.js
@@ -162,7 +162,60 @@ app.post ( '/insert-todb/one', ( req, res) => {
 //INSERTING MANY RECORDS INTO TABLE:
 app.post ( '/insert-todb/many', ( req, res) => {
     const multipleDataToInsert = {...req.body.multipleData};
-    res.json(multipleDataToInsert)
+    for ( let data in multipleDataToInsert) {
+        db.query ( 'SHOW DATABASES', ( err, rows) => {
+            if ( err) {
+                res
+                .status ( 500 )
+                .json ( {
+                    status: 'error',
+                    error: err.message,
+                });
+                return;
+            };
+            const dbToBeModified = rows.filter ( (row) => {
+                return row.Database === req.body.database;
+            } );
+            db.query ( ` SHOW TABLES FROM ${dbToBeModified[0].Database}`, ( err, rows) => {
+                if ( err) {
+                    res
+                    .status ( 500)
+                    .json ( {
+                        status: 'error',
+                        error: err.message,
+                    });
+                    return;
+                };
+                const tableToBeModified = rows.filter ( (row) => {
+                    for ( let prop in row ) {
+                        return row[prop] === req.body.dbTable;
+                    };
+                } );
+                for ( let prop in tableToBeModified[0] ) {
+                    db.query ( ` INSERT INTO ${dbToBeModified[0].Database}.${tableToBeModified[0][prop]} SET?`,
+                    multipleDataToInsert[data], ( err, rows) => {
+                        if ( err) {
+                            res
+                            .status ( 500)
+                            .json ( {
+                                status: 'error',
+                                error: err.message,
+                            });
+                            return;
+                        };
+                        res
+                        .status (500)
+                        .json ( {
+                            status: 'success',
+                            message: 'Data successfully saved to the database!',
+                            data: rows,
+                        } );
+                    }
+                    );
+                };
+            });
+        });
+    };   
 });
 
 
